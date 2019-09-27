9a956732cb7fbd3b70673ec80d19b6dc156139f0
hsinpaohuang <46573439+hsinpaohuang@users.noreply.github.com>
2019-09-26 22:15:05 -0400

    Add files via upload

--- drawmap.js
+++ drawmap.js
@@ -1,52 +1,43 @@
-var tip = d3.tip().attr('class', 'd3-tip e').html(function(d) { return '<strong>County: </strong> <span class="details">' + d.properties.NAME + '<br /></span> <strong>Percent bachelor\'s degree or higher:</strong><span class="details">' + d.properties.Percent + '%</span>'; });
+var tip;
 
+var print;
 var percent_data = [];
 var data = d3.map();
 var colorScheme = d3.schemeReds[5];
 colorScheme.unshift("#eee")
 var colorScale = d3.scaleThreshold()
-    .domain([0, 10, 30, 40, 50, 60])
+    .domain([0, 10, 20, 30, 40, 50])
 	.range(colorScheme);
-	
-var xx,
-	nest = {};
 
-var width = 200,
+var width = 250,
 	height = 300;
 
-var svg = d3.select("#map-div")
+var svg;
+function INmap(year)
+{
+	tip = d3.tip().attr('class', 'd3-tip').html(function(d) { return '<strong>County: </strong> <span class="details">' + d.properties.NAME + '<br /></span> <strong>Percent bachelor\'s degree or higher: </strong><span class="details">' + d.properties.Percent + '%</span>'; });
+	d3.select("#map-div > svg > g").selectAll("*").remove();
+	svg = d3.select("#map-div")
             .append("svg")
 			.attr("class", "map")
             .attr("width", width)
-            .attr("height", height);
+			.attr("height", height);
+	svg.call(tip);
+	tip.direction('e');
 
-svg.call(tip);
+	percent_data = [];
+	d3.queue()
+	.defer(d3.json,"indycounty.json")
+	.defer(d3.csv, "percent/p_"+year+".csv", function(d)
+		{
+			percent_data.push(d);
+		})//d3.csv
+		.await(ready);
+}//INmap
 
-d3.queue()
-  .defer(d3.json,"indycounty.json")
-  .defer(d3.csv, "percent/p_"+year+".csv", function(d){
-	/*
-	d.LaborForce = +d.LaborForce;
-	d.LabWorkRatio = +d.LabWorkRatio;
-	d.Workforce = +d.Workforce;
-	*/
-	percent_data.push(d);
-	})//d3.csv
-	.await(ready)
-/*
-d3.queue()
-  .defer(d3.json,"indycounty.json")
-  .defer(d3.csv,"in_out_2016.csv", function(d){
-		d.LaborForce = +d.LaborForce;
-		d.LabWorkRatio = +d.LabWorkRatio;
-		d.Workforce = +d.Workforce;
-	nest[d.description] = {name: d.description, labor: d.LaborForce, ratio: d.LabWorkRatio, work: d.Workforce};
-	})//d3.csv
-	.await(ready)
-	*/
-  function ready(error, countyMap)
-  {
-	console.log(percent_data);
+function ready(error, countyMap)
+{
+	console.log(percent_data[0].Percent);
 	if (error) throw error;
 
 	for(var i = 0; i < 92; i++)
@@ -60,52 +51,168 @@ d3.queue()
 			}//if
 		}//inner for
 	}//outer for
-	  
-    var indyState = topojson.feature(countyMap, {
-          type: "GeometryCollection",
-          geometries: countyMap.objects.indycounty.geometries
-      });//topojson.feature
+	
+	var indyState = topojson.feature(countyMap, {
+			type: "GeometryCollection",
+			geometries: countyMap.objects.indycounty.geometries
+		});//topojson.feature
 
-    var projection = d3.geoMercator()
-        .fitExtent([[0, 0], [width, height]], indyState);
+	var projection = d3.geoMercator()
+		.fitExtent([[0, 0], [width, height]], indyState);
 
-    var geoPath = d3.geoPath()
-                    .projection(projection);
-  // Draw the map
-var originalColor;
+	var geoPath = d3.geoPath()
+					.projection(projection);
+	// Draw the map
+	var originalColor;
 
- svg.append("g")
-     .attr("class", "countries")
-     .selectAll("path")
-     .data(indyState.features)
-     .enter().append("path")
-     .attr("stroke","black")
-     .attr("fill", function (d){
-		 	//console.log(d);
-		 	/*
-		 	d.data = nest[d.properties];
-            d.total = data.get(d.properties.NAME) || 0;
-	 		d.labor = data.get(d.properties.NAME);
-			 d.data = nest[d.properties.NAME];
-			 */
-			return colorScale(d.properties.Percent);
-        })//attr.fill
-	  .attr("d", geoPath)
-	  .on('mouseover', function(d){
-		originalColor = d3.select(this).style("fill");
-		d3.select(this)
-			.style('fill', 'Turquoise');
-		})//onmouseover
-	  .on('click', function(d) {
-	 	xx = -50;
-	 	d3.selectAll("rect").remove();
-	 	d3.selectAll("text").remove();
-		tip.show(d);
-		console.log(d);
-		changeCounty(d.properties.NAME);
- 		})//onclick
-	  .on('mouseout', function(d){
-	 		tip.hide();	
-			d3.select(this).style('fill', originalColor);
-	  	});//onmouseout
+	svg.append("g")
+		.attr("class", "countries")
+		.selectAll("path")
+		.data(indyState.features)
+		.enter().append("path")
+		.attr("stroke","black")
+		.attr("fill", function (d){
+				return colorScale(d.properties.Percent);
+			})//attr.fill
+		.attr("d", geoPath)
+		.on('mouseover', function(d){
+			originalColor = d3.select(this).style("fill");
+			d3.select(this)
+				.style('fill', 'Turquoise');
+			})//onmouseover
+		.on('click', function(d) {
+			tip.show(d);
+			changeCounty(d.properties.NAME);
+			})//onclick
+		.on('mouseout', function(d){
+				tip.hide();	
+				d3.select(this).style('fill', originalColor);
+			});//onmouseout
   }//ready
+
+//legend
+d3.select('body')
+.append('div')
+.attr('class', 'legend')
+.append('h3')
+.text("Legend")
+
+d3.select('div.legend')
+.append('svg')
+.attr('class', 'legend')
+.attr('id', 'zero')
+.attr('width', 15)
+.attr('height', 15)
+.append('circle')
+.attr('cx', 7.5)
+.attr('cy', 7.5)
+.attr('r', 5)
+.style('fill', colorScheme[0])
+.attr('stroke', 'black');
+
+d3.select('div.legend')
+.append('p')
+.attr('class', 'legend')
+.attr('id', 'zerot')
+.text('0% ~ 9%');
+
+d3.select('div.legend')
+.append('svg')
+.attr('class', 'legend')
+.attr('id', 'zero')
+.attr('width', 15)
+.attr('height', 15)
+.append('circle')
+.attr('cx', 7.5)
+.attr('cy', 7.5)
+.attr('r', 5)
+.style('fill', colorScheme[1])
+.attr('stroke', 'black');
+
+d3.select('div.legend')
+.append('p')
+.attr('class', 'legend')
+.attr('id', 'zerot')
+.text('10% ~ 19%');
+
+d3.select('div.legend')
+.append('svg')
+.attr('class', 'legend')
+.attr('id', 'fiveK')
+.attr('width', 15)
+.attr('height', 15)
+.append('circle')
+.attr('cx', 7.5)
+.attr('cy', 7.5)
+.attr('r', 5)
+.style('fill', colorScheme[2])
+.attr('stroke', 'black');
+
+d3.select('div.legend')
+.append('p')
+.attr('class', 'legend')
+.attr('id', 'fivekt')
+.text('20% ~ 39%');
+
+d3.select('div.legend')
+.append('svg')
+.attr('class', 'legend')
+.attr('id', 'tenK')
+.attr('width', 15)
+.attr('height', 15)
+.append('circle')
+.attr('cx', 7.5)
+.attr('cy', 7.5)
+.attr('r', 5)
+.style('fill', colorScheme[3])
+.attr('stroke', 'black');
+
+d3.select('div.legend')
+.append('p')
+.attr('class', 'legend')
+.attr('id', 'tenkt')
+.text('30% ~ 39%');
+
+d3.select('div.legend')
+.append('svg')
+.attr('class', 'legend')
+.attr('id', 'fifteenK')
+.attr('width', 15)
+.attr('height', 15)
+.append('circle')
+.attr('cx', 7.5)
+.attr('cy', 7.5)
+.attr('r', 5)
+.style('fill', colorScheme[4])
+.attr('stroke', 'black');
+
+d3.select('div.legend')
+.append('p')
+.attr('class', 'legend')
+.attr('id', 'fifteenkt')
+.text('40% ~ 49%');
+
+d3.select('div.legend')
+.append('svg')
+.attr('class', 'legend')
+.attr('id', 'twentyK')
+.attr('width', 15)
+.attr('height', 15)
+.append('circle')
+.attr('cx', 7.5)
+.attr('cy', 7.5)
+.attr('r', 5)
+.style('fill', colorScheme[5])
+.attr('stroke', 'black');
+
+d3.select('div.legend')
+.append('p')
+.attr('class', 'legend')
+.attr('id', 'twentykt')
+.text('50%+');
+
+//county info
+d3.select('body')
+.append('div')
+.attr('id', 'info')
+.append('p')
